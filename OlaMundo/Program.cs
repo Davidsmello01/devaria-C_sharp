a10c34aa091dda1979c1ced4e32c32b62a6e82f9
David Sena de Mello <83793130+Davidsmello01@users.noreply.github.com>
2021-06-01 21:24:36 -0300

    meu primeiro commit csharp

---
+++ OlaMundo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OlaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! my first code c# Devaria 2021");
+        }
+    }
+}
---
+++ TesteVariavelConstante/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TesteVariavelConstante
+{
+    class Program
+    {
+        static void Main(string[] argumentos)
+        {
+            const string constante = "uma constante que não muda";
+
+            if (argumentos.Length == 0)
+            {
+                Console.WriteLine("Favor informar numero inteiro");
+                return;
+            }
+            int inteiro;
+            bool test = int.TryParse(argumentos[0], out inteiro);
+
+            if (test == false)
+            {
+                Console.WriteLine("O argumrnto informado não é um numero inteiro");
+                return;
+            }
+
+            Console.WriteLine(constante);
+            Console.WriteLine("O numero informado foi: " + inteiro);
+        }
+    }
+}
